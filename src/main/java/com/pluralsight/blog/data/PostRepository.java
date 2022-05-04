7119aa594bbf5e1faa7bcceefc7cb1295e2cede9
hannawehrman <104872713+hannawehrman@users.noreply.github.com>
2022-05-03 23:44:51 -0500

    Update PostRepository.java

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,8 +1,8 @@
 package com.pluralsight.blog.data;
 
+import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
