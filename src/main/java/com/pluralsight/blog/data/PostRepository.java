353a775caa6176cc12073f86c033a762c6e77482
hannawehrman <104872713+hannawehrman@users.noreply.github.com>
2022-05-03 22:17:07 -0500

    Update PostRepository.java

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -1,8 +1,8 @@
 package com.pluralsight.blog.data;
 
-import com.pluralsight.blog.model.Author;
 import com.pluralsight.blog.model.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
@@ -27,3 +27,7 @@ public class PostRepository {
         return null;
     }
 }
+public interface PostRepository extends JpaRepository<Post, Long> {
+        @RestResource(rel="contains-title", path="containsTitle")
+        public List<Post> findByTitleContaining(String title);
+    }
